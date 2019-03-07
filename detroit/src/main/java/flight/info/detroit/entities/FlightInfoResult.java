6c018b3933d4930eebc2da8ca7a4320f8e7cf218
derekquinn <quinnderek@yahoo.com>
2019-03-07 18:12:43 -0500

    Flightinfo and project structure updates

---
+++ detroit/src/main/java/flight/info/detroit/entities/FlightInfoResult.java
@@ -0,0 +1,17 @@
+package flight.info.detroit.entities;
+
+import java.util.List;
+
+public class FlightInfoResult {
+
+	private List<Flights> flights;
+
+	public List<Flights> getFlights() {
+		return flights;
+	}
+
+	public void setFlights(List<Flights> flights) {
+		this.flights = flights;
+	}
+
+}
