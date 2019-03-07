be24734cb7ea1789dfee74ac7d54d6561805e0a1
derekquinn <quinnderek@yahoo.com>
2019-03-07 16:12:17 -0500

    Initial commit, hello world

---
+++ detroit/src/main/java/flight/info/detroit/MyFlightApplication.java
@@ -0,0 +1,13 @@
+package flight.info.detroit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFlightApplication.class, args);
+	}
+
+}
---
+++ detroit/src/main/java/flight/info/detroit/MyFlightController.java
@@ -0,0 +1,20 @@
+package flight.info.detroit;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class MyFlightController {
+	
+	@RequestMapping("/")
+	public ModelAndView list() {
+
+		return new ModelAndView("index");
+	}
+	
+
+}
