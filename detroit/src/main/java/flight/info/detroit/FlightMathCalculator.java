9a7e1e47073546abb619ba04b1b331da903681ec
derekquinn <quinnderek@yahoo.com>
2019-03-14 13:14:11 -0400

    conditional logic for bags

--- detroit/src/main/java/flight/info/detroit/FlightMathCalculator.java
+++ detroit/src/main/java/flight/info/detroit/FlightMathCalculator.java
@@ -7,7 +7,7 @@ import java.time.temporal.ChronoUnit;
 
 public class FlightMathCalculator {
 
-	private static Long getBagsTime = 20L;
+	private static Long getBagsTime = 22L;
 	private static Long getWalkToDoor = 15L;
 	
 	public static Long gateArrivalMath(FlightStatus fs) {
@@ -45,56 +45,10 @@ public class FlightMathCalculator {
 
 		long totalMinutes = minutes + hoursAsMinutes;
 
-		// String hoursAsString = Long.toString(hours);
-		// String minutesAsString = Long.toString(minutes);
-		// String hoursAndMinutes = "Difference is" + "HR " + hoursAsString + "MIN"+
-		// minutesAsString;
-
 		return totalMinutes;
 	}
-
-	public static Long runwayDepartureMath(FlightStatus fs) {
-
-		String estimateTakeoff = fs.getOperationalTimes().getEstimatedRunwayDeparture().getDateLocal();
-		String actualTakeoff = fs.getOperationalTimes().getActualRunwayDeparture().getDateLocal();
-		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
-		
-		LocalDateTime actual = LocalDateTime.parse(actualTakeoff, formatter);
-		LocalDateTime estimated = LocalDateTime.parse(estimateTakeoff, formatter);
-		
-		LocalDateTime fromTemp = LocalDateTime.from(actual);
-		
-		long years = fromTemp.until(estimated, ChronoUnit.YEARS);
-		fromTemp = fromTemp.plusYears(years);
-
-		long months = fromTemp.until(estimated, ChronoUnit.MONTHS);
-		fromTemp = fromTemp.plusMonths(months);
-
-		long days = fromTemp.until(estimated, ChronoUnit.DAYS);
-		fromTemp = fromTemp.plusDays(days);
-
-		long hours = fromTemp.until(estimated, ChronoUnit.HOURS);
-		fromTemp = fromTemp.plusHours(hours);
-
-		long minutes = fromTemp.until(estimated, ChronoUnit.MINUTES);
-		fromTemp = fromTemp.plusMinutes(minutes);
-
-		long seconds = fromTemp.until(estimated, ChronoUnit.SECONDS);
-		fromTemp = fromTemp.plusSeconds(seconds);
-		
-		//Not being used from the api, but place if the api useds it in future
-		long millis = fromTemp.until(estimated, ChronoUnit.MILLIS);
-
-		long hoursAsMinutes = hours * 60;
-
-		long totalMinutes = minutes + hoursAsMinutes;
-
 	
-		return totalMinutes;
-		
-	}
-	public static LocalDateTime leaveOrginTime(FlightStatus fs, Long durationInSeconds) {
+	public static LocalDateTime driverDepartureWithBags(FlightStatus fs, Long durationInSeconds) {
 		
 		String estimatedGateArrival = fs.getOperationalTimes().getEstimatedGateArrival().getDateLocal();
 		
@@ -110,5 +64,66 @@ public class FlightMathCalculator {
 		
 		
 		return timeToLeave;
-	} 
+	}
+	
+public static LocalDateTime driverDepartureNoBags(FlightStatus fs, Long durationInSeconds) {
+		
+		String estimatedGateArrival = fs.getOperationalTimes().getEstimatedGateArrival().getDateLocal();
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
+		LocalDateTime estimated = LocalDateTime.parse(estimatedGateArrival, formatter);
+		Long minsInTraffic = durationInSeconds/60;
+		
+		Long airlinePassTask = (getWalkToDoor);
+		
+		LocalDateTime timeAtDoor = estimated.plusMinutes(airlinePassTask);
+		LocalDateTime timeToLeave= timeAtDoor.minusMinutes(minsInTraffic);
+		
+		
+		
+		return timeToLeave;
+	}
+
+//	public static Long runwayDepartureMath(FlightStatus fs) {
+//
+//		String estimateTakeoff = fs.getOperationalTimes().getEstimatedRunwayDeparture().getDateLocal();
+//		String actualTakeoff = fs.getOperationalTimes().getActualRunwayDeparture().getDateLocal();
+//		
+//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
+//		
+//		LocalDateTime actual = LocalDateTime.parse(actualTakeoff, formatter);
+//		LocalDateTime estimated = LocalDateTime.parse(estimateTakeoff, formatter);
+//		
+//		LocalDateTime fromTemp = LocalDateTime.from(actual);
+//		
+//		long years = fromTemp.until(estimated, ChronoUnit.YEARS);
+//		fromTemp = fromTemp.plusYears(years);
+//
+//		long months = fromTemp.until(estimated, ChronoUnit.MONTHS);
+//		fromTemp = fromTemp.plusMonths(months);
+//
+//		long days = fromTemp.until(estimated, ChronoUnit.DAYS);
+//		fromTemp = fromTemp.plusDays(days);
+//
+//		long hours = fromTemp.until(estimated, ChronoUnit.HOURS);
+//		fromTemp = fromTemp.plusHours(hours);
+//
+//		long minutes = fromTemp.until(estimated, ChronoUnit.MINUTES);
+//		fromTemp = fromTemp.plusMinutes(minutes);
+//
+//		long seconds = fromTemp.until(estimated, ChronoUnit.SECONDS);
+//		fromTemp = fromTemp.plusSeconds(seconds);
+//		
+//		//Not being used from the api, but place if the api useds it in future
+//		long millis = fromTemp.until(estimated, ChronoUnit.MILLIS);
+//
+//		long hoursAsMinutes = hours * 60;
+//
+//		long totalMinutes = minutes + hoursAsMinutes;
+//
+//	
+//		return totalMinutes;
+//		
+//	}
+ 
 }
--- detroit/src/main/java/flight/info/detroit/MyFlightController.java
+++ detroit/src/main/java/flight/info/detroit/MyFlightController.java
@@ -58,7 +58,7 @@ public class MyFlightController {
 
 	@RequestMapping("flightresults")
 	public ModelAndView showFlightResults(@RequestParam("flightcode") String flightCode,
-			@RequestParam("origin") String origin) {
+			@RequestParam("origin") String origin, @RequestParam(name = "bags", required = false) Boolean checkedBags) {
 
 		// split the flight search into airline and flight number for API url and accept
 		// flight numbers ranging from 3-6 characters in length
@@ -74,31 +74,34 @@ public class MyFlightController {
 		// send duration in seconds to the database
 		flightstatus.get(0).setDuration(dur);
 		flightTripDao.update(flightstatus.get(0));
+		
+		LocalDateTime driverDeptTime;
 
-		// Duration duration = new Duration(dur);
-		// Duration.findByDuration(dur);
-		// flightTripDao.createDuration(duration);
-		// System.out.println(duration);
+		if (checkedBags != null) {
+			// storing the calculated departure time for driver / user with checked bags
+			driverDeptTime = FlightMathCalculator.driverDepartureWithBags(flightstatus.get(0), dur);
+		} else {
+			
+			// storing the calculated departure time for driver / user with no checked bags
+			driverDeptTime = FlightMathCalculator.driverDepartureNoBags(flightstatus.get(0), dur);			
+		} 
 
-		// storing the calculated departure time for driver / user
-		LocalDateTime driverDeptTime = FlightMathCalculator.leaveOrginTime(flightstatus.get(0), dur);
 		// sending driver departure time to database
 		flightstatus.get(0).setDriverDeparture(driverDeptTime);
 		flightTripDao.update(flightstatus.get(0));
 		// storing the calcualted driver departure time in a string, reformatted for humans
 		String formattedDriverDeptTime = driverDeptTime.toLocalTime().format(DateTimeFormatter.ofPattern("hh:mm a"));
-		// sending reformatted driver departure time to database 
+		// sending reformatted driver departure time to database
 		flightstatus.get(0).setFmtDriverDepartureTime(formattedDriverDeptTime);
 		flightTripDao.update(flightstatus.get(0));
 		// sending airline passenger gate assignment to database
-		
-		
+
 		ModelAndView mav = new ModelAndView("flightresults", "flightstatus", flightstatus);
 
 		mav.addObject("traffic", dur);
 		mav.addObject("origlocation", origin);
 		mav.addObject("gatearrivalmetric", gateArrivalMetric);
-		// placing  reformatted driver departure time on jsp after reformatting to non-military time
+		// placing reformatted driver departure time on jsp after reformatting to 12hr time
 		mav.addObject("grounddepttime", formattedDriverDeptTime);
 
 		return mav;
